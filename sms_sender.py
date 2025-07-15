edf833fd3ecffd319a56d94b0ad49270e819d447
tinsae34 <63494037+tinsae34@users.noreply.github.com>
2025-07-15 08:56:56 -0900

    Update sms_sender.py

--- sms_sender.py
+++ sms_sender.py
@@ -114,7 +114,8 @@ def get_address_from_coordinates(lat, lon):
 
 def remove_keyboard(chat_id):
     keyboard = {"remove_keyboard": True}
-    send_message(chat_id, "✅ ", reply_markup=keyboard)
+    send_message(chat_id, "", reply_markup=keyboard)  
+
 
 
 def save_delivery(data):
